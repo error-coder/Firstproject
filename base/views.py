362a10dd0ffa448c7726e5e4cc8908ff0696fc24
kjw <kjw8971@naver.com>
2022-11-18 23:51:43 +0900

    23:51

--- base/views.py
+++ base/views.py
@@ -9,7 +9,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-
 from .models import Task
 
 class CustomLoginView(LoginView):
